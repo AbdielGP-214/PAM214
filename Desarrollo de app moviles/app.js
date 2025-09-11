79e3d68631fc83663c04e474e555953f6ceaea36
AbdielGP-214 <124050294@upq.edu.mx>
2025-09-11 12:31:30 -0600

    8. bucle while

--- Desarrollo de app moviles/app.js
+++ Desarrollo de app moviles/app.js
@@ -1,13 +1,7 @@
-let typecard = 'Credit Card';
-
-switch(typecard){
-    case 'debid card':
-        console.log('this is a debid card');
-        break;
-
-    case 'Credit Card':
-        console.log('This is a credit card');
-        break;
-    default:
-        console.log ('no card');
+//bucles
+let count = 1;
+// se pueden usar todos los comparadores
+while(count <= 50) {
+    console.log(count);
+    count ++;
 }
