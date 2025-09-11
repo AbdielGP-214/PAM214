3c32bbb224590e6179f73a856be8cf01042a1268
AbdielGP-214 <124050294@upq.edu.mx>
2025-09-10 18:37:32 -0600

    1.tipo de datos

---
+++ Desarrollo de app moviles/app.js
@@ -0,0 +1,45 @@
+// tipos de datos
+"Hola mundo" // string
+'Hola mundo' // string
+
+// number
+100000
+-2.3
+
+// boolean
+true 
+false
+
+// array
+['joe','ryan','martha']
+[1,2,3]
+[true,false,true,false]
+
+// object
+'ryan'
+70.4
+14
+true
+
+{
+    "username": 'ryan',
+    "score": 70.4,
+    "hours":14,
+    "proffesional":true
+    
+}
+
+{
+    "username": 'joe',
+    "score": 0.4,
+    "hours":1,
+    "proffesional":false
+    
+}
+
+// sig
+console.log('esto es una cadena');
+console.log('14.3');
+console.log('true');
+console.log([1,2,3,4]);
+console.log({"username": "ryan", "score":70.4});
