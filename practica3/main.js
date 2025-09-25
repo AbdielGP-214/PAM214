807d8346d93b8e1d3fdb2bf0c4d6d39c14b96f92
AbdielGP-214 <124050294@upq.edu.mx>
2025-09-25 17:12:12 -0600

    Practica 3) ejercicio C

--- practica3/main.js
+++ practica3/main.js
@@ -4,29 +4,48 @@
 //console.log(resultado);
 
 
-
+/*
 function verificarUsuario(usuario) {
-  
-  return new Promise((resolve, reject) => {
+ 
+    return new Promise((resolve, reject) => {
     if (usuario === "admin") {
-      
-      resolve("Acceso concedido");
+
+    resolve("Acceso concedido");
     } else {
-      
-      reject("Acceso denegado");
+    
+    reject("Acceso denegado");
     }
-  });
+    });
 }
 
 
 verificarUsuario("admin")
-  .then(res => console.log(res)) 
-  .catch(err => console.error(err));
+    .then(res => console.log(res)) 
+    .catch(err => console.error(err));
 
 
 verificarUsuario("Ivan")
-  .then(res => console.log(res))
-  .catch(err => console.error(err));
+    .then(res => console.log(res))
+    .catch(err => console.error(err));
+*/
 
 
+function SimularPeticionAPI(){
 
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve("Datos Recibidos Correctamente");
+        },5000);
+    });
+}
+
+async function ObtenerDatos() {
+    
+    let result = await SimularPeticionAPI();
+
+    console.log(result);
+    
+
+    }
+
+ObtenerDatos();
