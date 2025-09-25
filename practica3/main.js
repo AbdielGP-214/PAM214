4ac87df4e914837557fccef09622c34b3076074c
AbdielGP-214 <124050294@upq.edu.mx>
2025-09-25 16:54:25 -0600

    practica 3) ejercicio B

--- practica3/main.js
+++ practica3/main.js
@@ -1,4 +1,32 @@
-import { restar } from './utils.js';
+//import { restar } from './utils.js';
 
-const resultado = restar(10, 3);
-console.log(resultado);
+//const resultado = restar(590, 239);
+//console.log(resultado);
+
+
+
+function verificarUsuario(usuario) {
+  
+  return new Promise((resolve, reject) => {
+    if (usuario === "admin") {
+      
+      resolve("Acceso concedido");
+    } else {
+      
+      reject("Acceso denegado");
+    }
+  });
+}
+
+
+verificarUsuario("admin")
+  .then(res => console.log(res)) 
+  .catch(err => console.error(err));
+
+
+verificarUsuario("Ivan")
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
+
+
+
